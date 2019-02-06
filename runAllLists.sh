45dfe5d921e3acb104dcefaaea1fa2f03f2c2af5
Paul T. Edlefsen <pedlefsen@gmail.com>
2019-02-06 13:21:12 -0800

    list fixes

--- runAllLists.sh
+++ runAllLists.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for patient in  `ls -c1 ${1}/*.fasta | egrep --only "_[0-9]{5,}_" | tr -d "_" | sort -u`
+for patient in  `ls -c1 ${1}/*.list  | egrep --only "[0-9]+\.list" | egrep --only "[0-9]+"  | sort -u`
 do 
    sbatch -JPREAST_${patient} -N1 -t 06:00 --mail-user=tholzman --mail-type=FAIL ./runList.bash ${1} $patient 
 done
